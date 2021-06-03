ae0cde5c625ae9c912914ed9b636f7dd0b2dfa52
David Scott Moyer <poinky_m@hotmail.com>
2021-06-03 13:23:47 -0500

    Set up react routing, beginning main page layout.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <>
+      <div className="layout-grid">
+        <Switch>
+          <Route exact path="/"></Route>
+          <Route exact path="/signup"></Route>
+          <Route exact path="/login"></Route>
+          <Route exact path="/create"></Route>
+          <Route path="/post/:post-id"></Route>
+          <Route path="/account/:account-id"></Route>
+        </Switch>
+      </div>
     </>
   );
 }
