6c4994aad438cb68e2d02ec27b285b2111138aac
David Scott Moyer <poinky_m@hotmail.com>
2021-06-03 13:41:56 -0500

    Finished main page loadout.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ function App() {
   return (
     <>
       <div className="layout-grid">
-        <Switch>
+        {/* <Navbar /> */}
+        {<Switch>
           <Route exact path="/"></Route>
           <Route exact path="/signup"></Route>
           <Route exact path="/login"></Route>
           <Route exact path="/create"></Route>
           <Route path="/post/:post-id"></Route>
           <Route path="/account/:account-id"></Route>
-        </Switch>
+        </Switch>}
       </div>
     </>
   );
