e39bcfebe7d8fe60f4a5d168c10f7fcc103067c6
David Scott Moyer <poinky_m@hotmail.com>
2021-06-07 14:37:44 -0500

    Posts are now retrieved from the database. Added forgotten key property.

--- src/App.js
+++ src/App.js
@@ -32,6 +32,11 @@ function App() {
       } else {
         localStorage.removeItem("auto-login");
       }
+
+      (async () => {
+        const postReq = await fetch("http://localhost:5000/posts").then(response => response.json());
+        setPosts(postReq);
+      })();
     }
   }, []);
 
