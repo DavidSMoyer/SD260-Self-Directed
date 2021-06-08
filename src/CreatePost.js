5c02e72f4209ba3269b0b477563d12becb4a36e5
David Scott Moyer <poinky_m@hotmail.com>
2021-06-08 13:29:37 -0500

    Added private feature to posts.

--- src/CreatePost.js
+++ src/CreatePost.js
@@ -1,4 +1,4 @@
-import {TextField} from '@material-ui/core';
+import {TextField, Switch, FormControlLabel} from '@material-ui/core';
 import {useState} from 'react';
 import {useHistory} from 'react-router-dom';
 
@@ -6,6 +6,7 @@ function CreatePost({user}) {
   const [title, setTitle] = useState("");
   const [img, setImg] = useState("");
   const [content, setContent] = useState("");
+  const [postPrivate, setPostPrivate] = useState(false);
   const history = useHistory();
 
   const update = (e, func, max = 99999) => {
@@ -29,7 +30,8 @@ function CreatePost({user}) {
         name: user.username,
         img: user.imageURL,
         id: user.id
-      }
+      },
+      private: postPrivate
     }
 
     const submit = await fetch("http://localhost:5000/posts", {
@@ -51,6 +53,7 @@ function CreatePost({user}) {
       <TextField value={title} fullWidth label="Post Title" required onChange={(e) => update(e, setTitle, 120)} />
       <TextField value={img} fullWidth label="Post Image URL" onChange={(e) => update(e, setImg)} />
       <TextField value={content} fullWidth multiline rows={10} label="Post Content" required className="content-input" variant="filled" onChange={(e) => update(e, setContent, 1000)} />
+      <FormControlLabel control={<Switch checked={postPrivate} onChange={(e) => setPostPrivate(e.target.checked)} />} label="Private Post" labelPlacement="top" />
       <input type="submit" value="Submit" />
     </form>
   )
