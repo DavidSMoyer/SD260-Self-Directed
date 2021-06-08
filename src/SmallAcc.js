5f68820bdad1b2308284019c69aa24358a41c9b3
David Scott Moyer <poinky_m@hotmail.com>
2021-06-08 12:53:23 -0500

    SmallAccount object now gets realtime user image.

--- src/SmallAcc.js
+++ src/SmallAcc.js
@@ -1,11 +1,21 @@
 import { Avatar } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function SmallAcc({owner}) {
+  const [img, setImg] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const imgReq = await fetch(`http://localhost:5000/users/${owner.id}`).then(response => response.json());
+      setImg(imgReq.imageURL);
+    })();
+  })
+
   return (
     <Link to={`/user/${"placeholder"}`}>
       <p>{owner.name}</p>
-      <Avatar className="small-avatar" src={owner.img} >{owner.img === "" && owner.name[0].toUpperCase()}</Avatar>
+      <Avatar className="small-avatar" src={img} >{img === "" && owner.name[0].toUpperCase()}</Avatar>
     </Link>
   )
 }
