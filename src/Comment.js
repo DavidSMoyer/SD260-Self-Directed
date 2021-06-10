4602916649e71bf65318de5f9798d607703d5aa4
David Scott Moyer <poinky_m@hotmail.com>
2021-06-10 13:29:57 -0500

    Added likes and reply field to comments.

--- src/Comment.js
+++ src/Comment.js
@@ -1,19 +1,28 @@
 import SmallAcc from './SmallAcc.js';
+import {TextField} from '@material-ui/core';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 
 function Comment({type, message, owner, replies, likes}) {
   return (
     <>
       <span className={type === "comment" ? "comment" : "reply"}>
         <SmallAcc owner={owner} />
+        <span className="like">
+          <FavoriteBorderIcon />
+          <span>0</span>
+        </span>
         <span className="message">{message}</span>
-        <span className="options"></span>
       </span>
       {
-        (replies !== undefined && replies.length > 0 && type === "comment") && 
+        (type === "comment") && 
         <ul>
           {replies.map(reply => (
             <Comment type="reply" owner={reply.owner} message={reply.message} />
           ))}
+          <form className="reply-field">
+            <TextField placeholder="Reply" size="small" />
+          </form>
         </ul>
       }
     </>
