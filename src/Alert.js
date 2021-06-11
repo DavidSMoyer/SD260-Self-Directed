9a75d285955d958e55a93b846870babb08c634e7
David Scott Moyer <poinky_m@hotmail.com>
2021-06-11 15:07:36 -0500

    Alerts are displayed on the alert page.

--- src/Alert.js
+++ src/Alert.js
@@ -2,10 +2,10 @@ import {Link} from 'react-router-dom';
 
 function Alert({alert}) {
   return (
-    <li className={alert.seen ? "" : "new"}>
+    <li className={alert.seen ? "alert" : "alert new"}>
       <Link to={alert.redirect}>
         <h2>{alert.title}</h2>
-        <p>{alert.description}</p>
+        <p>{alert.message}</p>
       </Link>
     </li>
   )
