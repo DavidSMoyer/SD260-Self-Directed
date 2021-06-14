eb238538dcd417ab34abdb08c595366c863ae436
David Scott Moyer <poinky_m@hotmail.com>
2021-06-14 14:56:23 -0500

    Optimized posts of a user.

--- src/PostList.js
+++ src/PostList.js
@@ -6,29 +6,25 @@ function PostList({type, user, account, setUser, query, alert}) {
 
   useEffect(() => {
     (async () => {
-      let posts = await fetch("http://localhost:5000/posts").then(response => response.json());
-      posts.sort((post1, post2) => post2.postTime - post1.postTime);
-      if (type === "main") {
+      if (type !== "owned") {
+        let posts = await fetch("http://localhost:5000/posts").then(response => response.json());
+        posts.sort((post1, post2) => post2.postTime - post1.postTime);
+        if (type === "follow") {
+          posts = posts.filter(post => user.following.includes(post.owner.id));
+        } else if (type === "search") {
+          posts = posts.filter(post => post.title.toLowerCase().includes(query.toLowerCase()) || post.content.toLowerCase().includes(query.toLowerCase()));
+        }
         posts = posts.filter(post => post.owner.id !== user.id);
-      } else if (type === "follow") {
-        posts = posts.filter(post => user.following.includes(post.owner.id));
-      } else if (type === "owned") {
-        posts = posts.filter(post => post.owner.id === account.id);
-      } else if (type === "search") {
-        console.log(posts);
-        posts = posts.filter(post => post.owner.id !== user.id);
-        console.log(posts);
-        posts = posts.filter(post => post.title.toLowerCase().includes(query.toLowerCase()) || post.content.toLowerCase().includes(query.toLowerCase()));
-        console.log(posts);
-      }
-
-      if (type !== "owned" || account.id !== user.id) {
-        const postCheck = posts.map(post => fetch(`http://localhost:5000/users/${post.owner.id}`));
-        const owners = await Promise.all(postCheck).
-          then(responses => Promise.all(responses.map(response => response.json())));
-        setPostList(posts.filter((post, idx) => !post.private || owners[idx].following.includes(user.id)));
+        const ownerList = posts.map(post => fetch(`http://localhost:5000/users/${post.owner.id}`));
+        await Promise.all(ownerList).then(responses => {
+          Promise.all(responses.map(response => response.json())).then(owners => {
+            posts = posts.filter((post, idx) => !post.private || (post.owner.id === user.id || owners[idx].following.includes(user.id)));
+            setPostList(posts);
+          })
+        });
       } else {
-        setPostList(posts);
+        let posts = await fetch(`http://localhost:5000/posts?owner.id=${account.id}`).then(response => response.json());
+        setPostList(posts.filter(post => !post.private || (account.following.includes(user.id) || user.id === account.id)));
       }
     })();
   }, [account, type, query])
