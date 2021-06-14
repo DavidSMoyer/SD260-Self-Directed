59b1163f325434e4b9679e14f29e9424d5af66a2
David Scott Moyer <poinky_m@hotmail.com>
2021-06-14 15:10:36 -0500

    Users can only see private posts if they and the owner both follow each other.

--- src/PostList.js
+++ src/PostList.js
@@ -18,13 +18,13 @@ function PostList({type, user, account, setUser, query, alert}) {
         const ownerList = posts.map(post => fetch(`http://localhost:5000/users/${post.owner.id}`));
         await Promise.all(ownerList).then(responses => {
           Promise.all(responses.map(response => response.json())).then(owners => {
-            posts = posts.filter((post, idx) => !post.private || (post.owner.id === user.id || owners[idx].following.includes(user.id)));
+            posts = posts.filter((post, idx) => !post.private || (post.owner === user.id || (owners[idx].following.includes(user.id) && user.following.includes(owners[idx].id))));
             setPostList(posts);
           })
         });
       } else {
         let posts = await fetch(`http://localhost:5000/posts?owner.id=${account.id}`).then(response => response.json());
-        setPostList(posts.filter(post => !post.private || (account.following.includes(user.id) || user.id === account.id)));
+        setPostList(posts.filter(post => !post.private || ((account.following.includes(user.id) && user.following.includes(account.id))|| user.id === account.id)));
       }
     })();
   }, [account, type, query])
